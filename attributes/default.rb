dd8dab15f1d0a597385ffbc758f343642b5463b3
Gaetano Giacalone <dagman62@gmail.com>
2018-08-18 10:50:53 -0700

    updated templates and attrubutes for nginx lb

---
+++ attributes/default.rb
@@ -0,0 +1,13 @@
+
+platform = node['platform']
+if platform == 'centos'
+  node.default['distro'] = node['platform']
+elsif platform == 'fedora'
+  node.default['distro'] = 'rhel'
+else
+  node.default['distro'] = node['platform']
+end
+
+node.default['nginx']['web1'] = 'dragon.example.com'
+node.default['nginx']['web2'] = 'shadow.example.com'
+node.default['nginx']['web3'] = 'hornet.example.com'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,99 @@
 #
-# Cookbook:: nginx-lb
+# Cookbook:: nginx
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+log "Your Platform is, #{node["distro"]}!" do
+  level :info
+end
+
+platform = node['platform']
+
+if platform == 'centos' || platform == 'fedora'
+  template '/etc/yum.repos.d/nginx.repo' do
+    source 'nginx.repo.erb'
+      variables ({
+        :distro => node['distro']
+      })
+      action :create
+  end
+end
+
+bash 'Update cache and Update' do
+  code <<-EOH
+  yum makecache && yum update -y
+  touch /tmp/updated
+  EOH
+  action :run
+  not_if { File.exist?('/tmp/updated') }
+end
+
+package 'nginx' do
+  action :install
+end
+
+bash 'Create Sites Directories' do
+  code <<-EOH
+  mkdir /etc/nginx/sites-enabled
+  mkdir /etc/nginx/sites-available
+  touch /tmp/directories
+  EOH
+  action :run
+  not_if { File.exist?('/tmp/directories') }
+end
+
+cookbook_file '/etc/nginx/sites-available/default.conf.save' do
+  source 'default.conf'
+  owner 'root'
+  group 'root'
+  mode '0644'
+  action :create
+end
+
+cookbook_file '/etc/nginx/nginx.conf' do
+  source 'nginx.conf'
+  owner 'root'
+  group 'root'
+  mode '0644'
+  action :create
+end
+
+template '/etc/nginx/sites-available/default.conf' do
+  source 'default.conf.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+  variables ({
+    :staticname => node['staticname'],
+    :fqdn       => node['fqdn'],
+    :host       => node['hostname'],
+    :port       => node['port'],
+  })
+  action :create
+end
+
+link '/etc/nginx/sites-enabled/default.conf' do
+  to '/etc/nginx/sites-available/default.conf'
+  link_type :symbolic
+end
+
+template '/usr/share/nginx/html/index.html' do
+  source 'index.html.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+  variables ({
+    :fqdn => node['fqdn'],
+  })
+  action :create
+end
+
+service 'nginx' do
+  action [:start, :enable]
+end
+
+
+
+
+
